e092f76528a68ca34c9aa2b4897b5baaee5f6387
Rodlyn Mahilum <123444084+RodlynSilent@users.noreply.github.com>
2023-05-21 23:55:42 +0800

    Add files via upload

---
+++ Faburada_Final_Project/src/main/java/com/csit228g3/faburada/faburada_final_project/Faburada_Final_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.csit228g3.faburada.faburada_final_project;
+
+/**
+ *
+ * @author emman
+ */
+public class Faburada_Final_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
